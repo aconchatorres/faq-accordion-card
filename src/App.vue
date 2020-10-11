2f0b6c834f449447ef847062639c3a71fad93485
Andres Concha <aconchatorres@hotmail.com>
2020-10-10 21:43:37 -0500

    background and white pill ready and centered

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FAQ/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FAQ from './components/FAQ.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FAQ
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap');
+
+html {
+  overflow-y: auto !important;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Kumbh Sans', sans-serif;
+  color: hsl(238, 29%, 16%);
+  font-size: 16px;
 }
 </style>
---
+++ src/components/FAQ.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="FAQ">
+    <div class="background-gradient">
+      <div class="white-pill column is-half">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FAQ'
+}
+</script>
+
+<style lang="scss" scoped>
+.FAQ {
+  .background-gradient {
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(hsl(273, 75%, 66%), hsl(240, 73%, 65%));
+    display: table-cell;
+    vertical-align: middle;
+  }
+  .white-pill {
+    width: 63vw;
+    height: 65vh;
+    background-color: white;
+    margin: auto;
+    border-radius: 25px;
+  }
+}
+</style>
